fe406280972c587edd1685a1231c0f706d208641
Ericn01 <nielseneric2001@gmail.com>
2024-11-16 15:51:48 -0700

    Polished the look a bit more and added some functionality.

--- MoodEntry.js
+++ MoodEntry.js
@@ -1,23 +1,49 @@
 class MoodEntry{
-    constructor(moodEmoji, moodValue, moodNotes){
+    constructor(moodValues){
+        this.moodEntryID = Math.floor(Math.random() * 10000) // Creates a random 5 digit integer
         // Setting up the default values for a mood entry object
         this.entryDate = new Date(Date.now()).toLocaleDateString('en-US', {year: 'numeric', month: 'long', day: 'numeric'}); // Creates the date in format Month Day, Year
-        this.moodEmoji = moodEmoji;
-        this.moodValue = moodValue;
-        this.moodNotes = moodNotes;
+        this.moodEmoji = moodValues['moodEmoji'];
+        this.moodValue = moodValues['moodScalar'];
+        this.moodNotes = moodValues['moodNotes'];
     }
 
-    handleNewMoodEntry(){
-        this.moodValuesHTML.forEach( (moodValue) => {
+    // Returns the mood entry data as an object
+    getMoodEntryData(){
+        return { 
+            moodEntryID: 
+            {
+                entryDate: this.entryDate,
+                entryMoodEmoji: this.moodEmoji,
+                entryMoodValue: this.moodValue,
+                entryMoodNotes: this.moodNotes
+            }
+        };
+    }
 
+    saveToLocalStorage(){
+        // Retrieve the data in local storage
+        const moodLogs = JSON.parse(localStorage.getItem('moodLogs'));
+        moodLogs.push(this.getMoodEntryData());
+        // Save the updated data in local storage
+        localStorage.setItem(JSON.stringify(moodLogs)); 
+    }
+
+    editMoodEntry(updatedMoodValues){
+        for (const entry of Object.entries(updatedMoodValues)){
+
+        }
+    }
+
+    renderMoodEntryLog(){
+        const moodEntryContainer = document.querySelector('.mood-entry-logs');
+        const entryValuesHTML = document.querySelectorAll('.log');
+
+        entryValuesHTML.forEach( (item) => {
+            
         })
     }
 
-
-
-    editMoodEntry(updatedMoodValues){
-
-    }
 }
 
 export default MoodEntry;
--- script.js
+++ script.js
@@ -1,36 +1,93 @@
-import MoodEntry from "./MoodEntry";
+import MoodEntry from "./MoodEntry.js";
 
 // Local storage retrieval of mood entries
+let moodLogs = {}; // empty dictionary for now
+if (localStorage.getItem('moodLogs')){ // Assuming that localStorage has been initialized
+    moodLogs = JSON.parse(localStorage.getItem('moodLogs'));
+} else{
+    localStorage.setItem('moodLogs', moodLogs); // Empty case
+};
 
+// Retrieving the desired HTML elements
+const moodValueSlider = document.querySelector('#mood-value')
+const moodValueModal = document.querySelector('#mood-value-modal');
+const moodNotes = document.querySelector('#mood-notes');
+const moodEmojiSelections = document.querySelectorAll('.mood-select');
+const warningMessage = document.querySelector('.warning-message');
+const moodEntryLogs = doucment.querySelector('.mood-entry-logs');
+const submitBtn = document.querySelector('#mood-submit-btn');
 
 const selectedMoodValues = {
-    emojiMood: [],
-    moodScalar: 0,
+    emojiMood: '',
+    moodScalar: parseFloat(moodValueSlider.value),
     moodNote: '',
+};
+
+const updateHTMLSelection = () => {
+
 };
 
 const updateMoodEmoji = (event) => {
     const emojiValue = event.target.id; // Name of the emoji
     const emojiText = event.target.textContent; // The actual emoji
+    // Change the background color of the element to know it's selected
+    moodEmojiSelections.forEach( (emojiSelection) => {
+        if (emojiSelection.id !== emojiValue){
+            emojiSelection.style.backgroundColor = '#FAF6E3';
+        } else{
+            event.target.style.backgroundColor = 'lightgreen';
+        }
+    })
     // Update the emoji mood
     selectedMoodValues['emojiMood'] = [emojiValue, emojiText];
 };
 
 // Add an event listener for clicks on mood emojis 
-document.querySelectorAll('.mood-select').forEach( (moodEmoji) => {
+moodEmojiSelections.forEach( (moodEmoji) => {
     moodEmoji.addEventListener('click', updateMoodEmoji);
 });
 
+
 // Event listener for mood value
-document.querySelector('#mood-value').addEventListener('input', (event) => {
-    selectedMoodValues['moodScalar'] = parseInt(event.target.value);
+moodValueSlider.addEventListener('input', (event) => {
+    // Update the mood scalar value
+    const moodScalar = parseFloat(event.target.value);
+    selectedMoodValues['moodScalar'] = moodScalar;
+    // Display the current scale value as a modal
+    moodValueModal.textContent = moodScalar;
 });
 
 // Event listener for 
-document.querySelector('#mood-notes').addEventListener('input', (event) => {
+moodNotes.addEventListener('input', (event) => {
     selectedMoodValues['moodNote'] = event.target.textContent;
 });
 
-document.querySelector('#mood-submit-btn').addEventListener('click', () => {
+submitBtn.addEventListener('click', () => {
+    if (len(selectedMoodValues['emojiMood']) > 0 && selectedMoodValues['moodScalar']){
+        setTimeout(() => {
+            submitBtn.disabled = true;
+            warningMessage.className = 'warning-message'; // Removes the hidden part
+            warningMessage.textContent = 'Please make sure to select an emoji mood and a mood value!'
+        }, 1500);
+        warningMessage.className = 'warning-message hidden';
+        submitBtn.disabled = false; // return the button to previous functionality
+    } else{
+        // Create a new mood entry
+        const newMoodEntry = new MoodEntry(selectedMoodValues);
+        newMoodEntry.saveToLocalStorage(); // Save the new entry to local storage
+        // Resets the values.
+        Object.values(selectedMoodValues).forEach( moodValue => {
+            if (typeof moodValue !== 'number'){
+                moodValue = '';
+            } else {
+                moodValue = 0;
+            }
+        }); 
+    }
+});
 
-})
+
+const renderMoodLogList = () => {
+    moodEntryLogs.innerHTML = ''; // Remove the previous content
+    localStorage.getItem('')
+}
